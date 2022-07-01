b2f70949d6d58b3b95ce707dc460e4a5df1725d1
cclouse4 <91483509+cclouse4@users.noreply.github.com>
2022-07-01 15:01:03 -0400

    Block styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'e39dc35587195b38a62c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '03a1179f1d63998f6a48');
