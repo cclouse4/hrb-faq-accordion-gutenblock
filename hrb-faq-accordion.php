1d40b9de2d098d91289c1c41fe1dd409de2bbe14
cclouse4 <91483509+cclouse4@users.noreply.github.com>
2022-07-01 10:56:03 -0400

    updates

--- hrb-faq-accordion.php
+++ hrb-faq-accordion.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Hrb Faq Accordion
- * Description:       Example static block scaffolded with Create Block tool.
+ * Description:       FAQ Accordion for HRB Block Advisors.
  * Requires at least: 5.9
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Cassie Clouse (Chemistry)
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       hrb-faq-accordion
@@ -13,14 +13,16 @@
  * @package           create-block
  */
 
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
 function create_block_hrb_faq_accordion_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_hrb_faq_accordion_block_init' );
+
+
+
+add_action('wp_enqueue_scripts','add_hrb_scripts');
+
+function add_hrb_scripts() {
+	wp_enqueue_script( 'hrb_faq_jquery', plugins_url( '/js/jquery-3.6.0.min.js', __FILE__ ));
+	wp_enqueue_script( 'hrb_faq_scripts', plugins_url( '/js/scripts.js', __FILE__ ), array('hrb_faq_jquery'));
+}
