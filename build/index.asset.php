afb7d3ff74f64bc18c066030fbc255be9bc2bc2a
cclouse4 <91483509+cclouse4@users.noreply.github.com>
2022-07-05 10:44:12 -0400

    updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '03a1179f1d63998f6a48');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '46545181b269404c6248');
