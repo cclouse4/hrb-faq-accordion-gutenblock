dddd1312dac53c5687d39f12004f26ec04aa0d65
cclouse4 <91483509+cclouse4@users.noreply.github.com>
2022-07-08 13:46:27 -0400

    Style updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '453585218084b70571bc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '13dceff6e234f312fb9e');
